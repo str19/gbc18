2b884b7606e0cde2fa7d371b6994add2505a9653
str19 <35856472+str19@users.noreply.github.com>
2018-01-27 17:51:27 -0500

    PlayerAnimation
    -Added Animation Folder
    -Created Animator Controller
    -Mapped 4 Directions

---
+++ Assets/Scripts/playerAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAnimation : MonoBehaviour {
+
+    public Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+    }
+}

8f37fafd2fd4a95b9d07fb427e9aa4c82181fa6f
str19 <35856472+str19@users.noreply.github.com>
2018-01-27 23:54:13 -0500

    island update
    -water level animation added to overworld
    -textures to overworld models
    -animation to camera
    -standard asset texture folders added

---
+++ Assets/Scripts/WaterDeath.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WaterDeath : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("hit");
+
+        if (collision.gameObject.name == "Player")
+        {
+
+            print("You die");
+            SceneManager.LoadScene("island");
+
+        }
+
+    }
+
+}
